05b94c0837a10b57415ffd21516778a0f315de19
ZGChung <69182806+ZGChung@users.noreply.github.com>
2021-10-22 09:01:12 +0200

    feat: add the framework for TestSeance4

--- src/main/java/org/centrale/projettp/TestSeance4.java
+++ src/main/java/org/centrale/projettp/TestSeance4.java
@@ -15,26 +15,55 @@ import java.lang.*;
  */
 public class TestSeance4 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         // creer un monde
         System.out.println("-------------------");
         System.out.println("   Hello World!");
         System.out.println("-------------------");
-        // System.out.println(" Create a world");
+        System.out.println("\nVeuillez choisir un mode de commencer :");
+        System.out.println(" 1 : Start a new game.");
+        System.out.println(" 2 : Load a game.");
+        int mode = 0;
+        while (mode != 1 && mode != 2) {
+            System.out.println("Veuillez entrer un numéro:");
+            mode = sc.nextInt();
+        }
+        switch (mode) {
+        case 1: // Start a new game
+            System.out.println("-------------------");
+            System.out.println(" Create a world");
+            System.out.println("-------------------");
+            World2 monde4 = new World2(30, 30);
+            monde4.creeMondeAlea(20, 5, 20, 1);
+            monde4.jouer();
+            break;
+        case 2: // Load a game
+            System.out.println("-------------------");
+            System.out.println("   Load a world!");
+            System.out.println("-------------------");
+            System.out.println("Please input the name of save file");
+            String saveName;
+            sc = new Scanner(System.in);
+            saveName = sc.nextLine();
+            System.out.println("The file you input is: " + saveName);
+            try {
+                ChargementPartie charge = new ChargementPartie(saveName);
+                // ChargementPartie charge = new ChargementPartie("Sauvegarde-WoE.txt");
+                World2 world = charge.chargerPartie();
+                world.jouer();
+
+            } catch (Exception e1) {
+                System.out.println("The file name that you entered was wrong.");
+            }
+            break;
+
+        }
+
         // System.out.println("-------------------");
-        // World2 monde4 = new World2(30, 30);
-        // monde4.creeMondeAlea(20, 5, 20, 1);
-        // monde4.jouer();
-
+        // System.out.println(" Save a world!");
+        // System.out.println("-------------------");
         System.out.println("-------------------");
-        System.out.println("   Save a world!");
+        System.out.println("  Game is closed.");
         System.out.println("-------------------");
-
-        System.out.println("-------------------");
-        System.out.println("   Load a world!");
-        System.out.println("-------------------");
-        ChargementPartie charge = new ChargementPartie("Sauvegarde-WoE.txt");
-        World2 world = charge.chargerPartie();
-        world.jouer();
     }
 }
